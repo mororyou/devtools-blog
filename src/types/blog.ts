1dc6e472009270abea36ad70b2a075a6b9007171
tomare-moroizumi <m.ryousuke0401@icloud.com>
2023-02-02 22:31:03 +0900

    Update blog.ts

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,21 +1,36 @@
-export type Blog = {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    revisedAt: string;
-    title: string;
-    content: string;
-    eyecatch: {
-        url: string;
-        height: number;
-        width: number;
-    };
-};
+export type blog = {
+  id: string
+  title: string
+  description: string
+  thumbnail: any
+  body: string
+  tags: tag[]
+  popular: boolean
+  relations: blogRelations[] | null
+  publishedAt: string
+  createdAt: string
+  updatedAt: string
+}
 
-export type BlogResponse = {
-    totalCount: number;
-    offset: number;
-    limit: number;
-    contents: Blog[];
-};
+export type blogRelations = {
+  title: string
+  url: string
+  external: boolean
+}
+
+export type blogThumbnail = {
+  url: string
+  height: number
+  width: number
+}
+
+export type BlogQuery = {}
+
+export type tag = {
+  id: string
+  name: string
+  revisedAt: string
+  publishedAt: string
+  createdAt: string
+  updatedAt: string
+}
