f111410d7d70f97e5542a82cea49efa029436737
tomare-moroizumi <m.ryousuke0401@icloud.com>
2023-02-03 02:27:42 +0900

    ヘッダー定数　パス誤り

--- src/constans/const.ts
+++ src/constans/const.ts
@@ -5,8 +5,8 @@ export const SITE_DOMAIN = 'http://devtools-blog.jp'
 export const SITE_WRITER = 'もろいー'
 
 export const HEADER_NAVI = [
-  { page: 'articles', title: '記事', subTitle: 'article' },
-  { page: 'portfolio', title: 'ポートフォリオ', subTitle: 'portfolio' },
-  { page: 'profile', title: 'プロフィール', subTitle: 'profile' },
-  { page: 'contact', title: 'コンタクト', subTitle: 'contact' },
+  { page: '/articles', title: '記事', subTitle: 'article' },
+  { page: '/portfolio', title: 'ポートフォリオ', subTitle: 'portfolio' },
+  { page: '/profile', title: 'プロフィール', subTitle: 'profile' },
+  { page: '/contact', title: 'コンタクト', subTitle: 'contact' },
 ]
