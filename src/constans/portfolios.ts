81dd2192a2e8767488e5060a735873865eedc401
tomare-moroizumi <m.ryousuke0401@icloud.com>
2023-02-02 22:30:24 +0900

    portfolio const setting

---
+++ src/constans/portfolios.ts
@@ -0,0 +1,30 @@
+export const PORTFOLIOS = [
+  {
+    key: 'tousekisyoku',
+    title: '透析食.com',
+    thumbnail: '',
+    url: 'https://tousekisyoku.com',
+    tech: ['Laravel', 'Next.js', 'Firebase'],
+  },
+  {
+    key: 'libecity',
+    title: 'イベント管理アプリ',
+    thumbnail: '',
+    url: '',
+    tech: ['Next.js', 'supabase'],
+  },
+  {
+    key: 'youtube',
+    title: 'Youtube 動画検索',
+    thumbnail: '',
+    url: '',
+    tech: ['Next.js', 'supabase', 'YoutubeDataApi'],
+  },
+  {
+    key: '',
+    title: '技術ブログ - Astro',
+    thumbnail: '',
+    url: '',
+    tech: ['Astro', 'MicroCMS'],
+  },
+]
