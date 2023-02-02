16198a448febe1fe819e18b908f7f05c984aaed1
tomare-moroizumi <m.ryousuke0401@icloud.com>
2023-02-03 02:28:45 +0900

    各種型変更

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,34 +1,59 @@
+// ブログ
 export type blog = {
   id: string
   title: string
+  path: string
   description: string
-  thumbnail: any
   body: string
+  thumbnail: blogThumbnail
   tags: tag[]
   popular: boolean
   relations: blogRelations[] | null
+  revisedAt: string
   publishedAt: string
   createdAt: string
   updatedAt: string
 }
 
-export type blogRelations = {
-  title: string
-  url: string
-  external: boolean
-}
-
+// ブログサムネ
 export type blogThumbnail = {
   url: string
   height: number
   width: number
 }
 
-export type BlogQuery = {}
+// 関連リンク
+export type blogRelations = {
+  title: string
+  url: string
+  external: boolean
+}
 
+// タグ
 export type tag = {
   id: string
   name: string
+  path: string
+  icon: iconThumbnail
+  revisedAt: string
+  publishedAt: string
+  createdAt: string
+  updatedAt: string
+}
+
+// アイコン
+export type iconThumbnail = {
+  url: string
+  height: number
+  width: number
+}
+
+// コメント
+export type comment = {
+  id: string
+  article_id: string
+  user: string
+  comment: string
   revisedAt: string
   publishedAt: string
   createdAt: string
