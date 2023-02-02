2e0a2c84bdc3374e8d1cef99c371cc77673b42fc
tomare-moroizumi <m.ryousuke0401@icloud.com>
2023-02-02 22:28:52 +0900

    Header Component Store

--- src/constans/const.ts
+++ src/constans/const.ts
@@ -1,5 +1,12 @@
-export const SITE_TITLE = 'SITE_TITLE'
+export const SITE_TITLE = 'techFountain'
 
 export const SITE_DOMAIN = 'http://devtools-blog.jp'
 
 export const SITE_WRITER = 'もろいー'
+
+export const HEADER_NAVI = [
+  { page: 'articles', title: '記事', subTitle: 'article' },
+  { page: 'portfolio', title: 'ポートフォリオ', subTitle: 'portfolio' },
+  { page: 'profile', title: 'プロフィール', subTitle: 'profile' },
+  { page: 'contact', title: 'コンタクト', subTitle: 'contact' },
+]
